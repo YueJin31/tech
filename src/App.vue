bc25e94a8c24e60514ab1b649029bf98ae324d40
YueJin <yuejin07@gmail.com>
2023-02-10 21:09:54 +0200

    push

---
+++ src/App.vue
@@ -0,0 +1,97 @@
+<template>
+  <div class="wrapper">
+    <div class="accordeon">
+      <AccordeonItem :title="'The analysis'">
+        <ul>
+          <li>Do you have one user in more than one variant of the same AB test?</li>
+          <li>Are you users actually getting the treatment we expect them to get?</li>
+          <li>Do we have enough users in our AB test? Are those users representative for the total population?</li>
+        </ul>
+      </AccordeonItem>
+
+      <AccordeonItem :title="'To take into account on user level data'">
+        <ul>
+          <li>Do you have one user in more than one variant of the same AB test?</li>
+          <li>Are you users actually getting the treatment we expect them to get?</li>
+          <li>Do we have enough users in our AB test? Are those users representative for the total population?</li>
+        </ul>
+      </AccordeonItem>
+
+      <AccordeonItem :title="'Our data'">
+        <ul>
+          <li>Do you have one user in more than one variant of the same AB test?</li>
+          <li>Are you users actually getting the treatment we expect them to get?</li>
+          <li>Do we have enough users in our AB test? Are those users representative for the total population?</li>
+        </ul>
+      </AccordeonItem>
+
+      <AccordeonItem :title="'Hands on analyzing'">
+        <ul>
+          <li>Do you have one user in more than one variant of the same AB test?</li>
+          <li>Are you users actually getting the treatment we expect them to get?</li>
+          <li>Do we have enough users in our AB test? Are those users representative for the total population?</li>
+        </ul>
+      </AccordeonItem>
+
+      <AccordeonItem :title="'Initialization'">
+        <ul>
+          <li>Do you have one user in more than one variant of the same AB test?</li>
+          <li>Are you users actually getting the treatment we expect them to get?</li>
+          <li>Do we have enough users in our AB test? Are those users representative for the total population?</li>
+        </ul>
+      </AccordeonItem>
+    </div>
+  </div>
+</template>
+
+<script>
+import AccordeonItem from "@/components/Item";
+export default {
+  components: {
+    AccordeonItem,
+  },
+};
+</script>
+
+<style lang="scss">
+@import url("https://fonts.googleapis.com/css2?family=Montserrat:wght@600;800&display=swap");
+
+body {
+  font-family: "Montserrat", sans-serif;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+  margin: 8px 0 0;
+
+  li {
+    margin-bottom: 8px;
+    position: relative;
+    padding-left: 20px;
+    line-height: 24px;
+
+    &::before {
+      content: "";
+      width: 10px;
+      height: 10px;
+      border: 2px solid #2b41b7;
+      border-radius: 50%;
+      position: absolute;
+      left: 0;
+      top: 3px;
+    }
+
+    &:last-child {
+      margin: 0;
+    }
+  }
+}
+
+.wrapper {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+}
+</style>
---
+++ src/components/Item.vue
@@ -0,0 +1,99 @@
+<template>
+  <div class="accordeon__item" :class="{ active: isActive }" @click="toggleAccordion">
+    <div class="accordeon__item-title">{{ title }}</div>
+    <Transition>
+      <div v-show="isOpen" class="accordeon__item-body">
+        <slot></slot>
+      </div>
+    </Transition>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    title: String,
+  },
+  data: () => ({
+    isOpen: false,
+    isActive: false,
+  }),
+  methods: {
+    toggleAccordion() {
+      this.isOpen = !this.isOpen;
+      this.isActive = !this.isActive;
+    },
+  },
+};
+</script>
+
+<style lang="scss">
+.accordeon {
+  background: #ffffff;
+  box-shadow: 0px 4px 8px rgba(0, 0, 0, 0.02), 0px 4px 17px rgba(0, 0, 0, 0.14), 0px 4px 22px rgba(0, 0, 0, 0.12);
+  border-radius: 8px;
+  width: 600px;
+
+  &:last-child {
+    border: none;
+  }
+
+  .accordeon__item.active {
+    background: #faf8f6;
+    transition: all 0.5s ease;
+
+    .accordeon__item-title {
+      color: #2b41b7;
+
+      &::after {
+        transform: rotate(315deg);
+      }
+    }
+  }
+
+  &__item {
+    padding: 16px 23px 16px;
+    border-bottom: 1px solid #d0dbf1;
+    cursor: pointer;
+
+    @media screen and (min-width: 768px) {
+      padding: 24px 31px 24px;
+    }
+
+    &-title {
+      color: #0d2a54;
+      font-size: 16px;
+      font-weight: 800;
+      line-height: 24px;
+      display: flex;
+      justify-content: space-between;
+
+      &::after {
+        content: "";
+        width: 10px;
+        height: 10px;
+        border-top: 2px solid #0d2a54;
+        border-right: 2px solid #0d2a54;
+        display: block;
+        transform: rotate(135deg);
+        transition: all 0.5s ease;
+      }
+    }
+
+    &-body {
+      font-size: 16px;
+      color: #0d2a54;
+    }
+  }
+}
+
+.v-enter-active,
+.v-leave-active {
+  transition: opacity 0.5s ease;
+}
+
+.v-enter-from,
+.v-leave-to {
+  opacity: 0;
+}
+</style>
